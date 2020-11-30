d6e49f6c340099429465b9c0f55f49c0c0a6dbe8
conan1207 <ksohee1207@gmail.com>
2020-12-01 02:20:12 +0900

    socket

--- JavaScript/socket/app.js
+++ JavaScript/socket/app.js
@@ -3,10 +3,10 @@
 
 // require를 쓰면 자동적으로 node module을 향함. 경로 없이 이름만 써도 됨.
 const express = require('express');
-const http = require('http') // node.js 의 core module.
+const http = require('http'); // node.js 의 core module.
 const app = express();
 const path = require('path'); // node.js 의 기본 module. url을 쉽게 만들 수 있게
-const server = http.createServer(app) // express로 만든 app을 server에 담아서 http로 실행될 수 있게.
+const server = http.createServer(app); // express로 만든 app을 server에 담아서 http로 실행될 수 있게.
 const socketIO = require('socket.io');
 const moment = require('moment');
 
@@ -20,18 +20,18 @@ app.use(express.static(path.join(__dirname, 'src')));
 const PORT = process.env.PORT || 5000;
 
 io.on('connection', (socket) => {
-    // socket.on('채팅아이디', 실행함수);
-    socket.on('chatting', (data) => {
-        const {name, msg} = data; //프론트에서 클릭 시 넘겨주는 데이터
-        io.emit('chatting',{
-            name,
-            msg,
-            time: moment(new Date()).format('HH:mm A')
-        });
+  // socket.on('채팅아이디', 실행함수);
+  socket.on('chatting', (data) => {
+    const { name, msg } = data; //프론트에서 클릭 시 넘겨주는 데이터
+    io.emit('chatting', {
+      name,
+      msg,
+      time: moment(new Date()).format('HH:mm A'),
     });
+  });
 });
 
 // 웹서버 실행 app.listen(포트, 명령) -> app이 server에 담김.
 server.listen(PORT, () => {
-    console.log(`server is running ${PORT}`);
+  console.log(`server is running ${PORT}`);
 });
--- JavaScript/socket/src/js/chat.js
+++ JavaScript/socket/src/js/chat.js
@@ -6,51 +6,51 @@ const nickName = document.querySelector('#nickName');
 const chatList = document.querySelector('.chatting-list');
 const chatInput = document.querySelector('.chatting-input');
 const sendBtn = document.querySelector('.send-button');
-const displayContainer = document.querySelector('.display-container')
+const displayContainer = document.querySelector('.display-container');
 
 // 'Enter' === 13
 chatInput.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter'){
-        send();
-        chatInput.value = '';
-    }
+  if (e.key === 'Enter') {
+    send();
+    chatInput.value = '';
+  }
 });
 
 sendBtn.addEventListener('click', send);
 
-function send(){
-    const param = {
-        name: nickName.value,
-        msg: chatInput.value
-    }
-    socket.emit('chatting', param); // 보낼 떄
+function send() {
+  const param = {
+    name: nickName.value,
+    msg: chatInput.value,
+  };
+  socket.emit('chatting', param); // 보낼 떄
 }
 
 socket.on('chatting', (data) => {
-    console.log(data)
-    const { name, msg, time } = data;
-    const item = new LiModel(name, msg, time);
-    item.makeLi();
-    displayContainer.scrollTo(0, displayContainer.scrollHeight);
-});// 받을 때
+  console.log(data);
+  const { name, msg, time } = data;
+  const item = new LiModel(name, msg, time);
+  item.makeLi();
+  displayContainer.scrollTo(0, displayContainer.scrollHeight);
+}); // 받을 때
 
-function LiModel(name, msg, time){
-    this.name = name;
-    this.msg = msg;
-    this.time = time;
+function LiModel(name, msg, time) {
+  this.name = name;
+  this.msg = msg;
+  this.time = time;
 
-    this.makeLi = () => {
-        const li = document.createElement('li');
-        li.classList.add(nickName.value === name ? "sent" : "received");
-        const dom = `
+  this.makeLi = () => {
+    const li = document.createElement('li');
+    li.classList.add(nickName.value === name ? 'sent' : 'received');
+    const dom = `
         <span class="profile">
           <span class="user">${this.name}</span>
           <img src="https://placeimg.com/50/50/any" alt="any" />
         </span>
         <span class="message">${this.msg}</span>
         <span class="time">${this.time}</span>
-      `
-      li.innerHTML = dom;
-      chatList.appendChild(li);
-    }
+      `;
+    li.innerHTML = dom;
+    chatList.appendChild(li);
+  };
 }
